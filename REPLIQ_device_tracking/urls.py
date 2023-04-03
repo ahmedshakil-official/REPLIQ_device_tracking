f79da422591dea03547ef8d9c48353068f5c8dfc
Ahmed Shakil <ahmedshakil.official@gmail.com>
2023-04-03 16:19:07 +0600

    Change Admin panel header and index title

--- REPLIQ_device_tracking/urls.py
+++ REPLIQ_device_tracking/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "REPLIQ_device_tracking Admin"
+admin.site.index_title = "Manage Your Data"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
