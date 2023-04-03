fdaa02f9a563280c3462083a44d69db6929b64b7
Ahmed Shakil <ahmedshakil.official@gmail.com>
2023-04-03 14:30:45 +0600

    Django restframework added and also register all model at admin panel

--- device_manager/admin.py
+++ device_manager/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.Company)
+admin.site.register(models.Employee)
+admin.site.register(models.CompanyManager)
+admin.site.register(models.Device)
+admin.site.register(models.DeviceLogInfo)
